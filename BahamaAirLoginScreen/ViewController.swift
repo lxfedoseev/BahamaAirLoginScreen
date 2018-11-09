a2c329d7c780c2c1711f40b6950202eedeb5b9f9
Alex Fedoseev <lxfedoseev@gmail.com>
2018-11-09 09:26:23 +0300

    chapter 12

--- BahamaAirLoginScreen/ViewController.swift
+++ BahamaAirLoginScreen/ViewController.swift
@@ -233,6 +233,21 @@ class ViewController: UIViewController {
     tintBackgroundColor(layer: loginButton.layer, toColor: tintColor)
     
     roundCorners(layer: loginButton.layer, toRadius: 25.0)
+    
+    let balloon = CALayer()
+    balloon.contents = UIImage(named: "balloon")!.cgImage
+    balloon.frame = CGRect(x: -50.0, y: 0.0, width: 50.0, height: 65.0)
+    view.layer.insertSublayer(balloon, below: username.layer)
+    let flight = CAKeyframeAnimation(keyPath: "position")
+    flight.duration = 12.0
+    flight.values = [
+        CGPoint(x: -50.0, y: 0.0),
+        CGPoint(x: view.frame.width + 50.0, y: 160.0),
+        CGPoint(x: -50.0, y: loginButton.center.y)
+        ].map { NSValue(cgPoint: $0) }
+    flight.keyTimes = [0.0, 0.5, 1.0]
+    balloon.add(flight, forKey: nil)
+    balloon.position = CGPoint(x: -50.0, y: loginButton.center.y)
   }
 
   // MARK: UITextFieldDelegate
@@ -300,6 +315,13 @@ class ViewController: UIViewController {
                         
                         roundCorners(layer: self.loginButton.layer, toRadius: 10.0)
         })
+        
+        let wobble = `CAKeyframeAnimation`(keyPath: "transform.rotation")
+        wobble.duration = 0.25
+        wobble.repeatCount = 4
+        wobble.values = [0.0, -.pi/4.0, 0.0, .pi/4.0, 0.0]
+        wobble.keyTimes = [0.0, 0.25, 0.5, 0.75, 1.0]
+        heading.layer.add(wobble, forKey: nil)
     }
     
     func animateCloud(layer: CALayer) {
